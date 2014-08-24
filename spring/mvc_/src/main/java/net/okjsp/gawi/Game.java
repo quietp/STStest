6302e9f05e1225dca6e180a7603915c397ac5b15
guorenzhen <guorenzhen@192.168.0.5>
2014-08-24 21:19:06 +0900

    Complete - task : View 분리

--- spring/mvc_/src/main/java/net/okjsp/gawi/Game.java
+++ spring/mvc_/src/main/java/net/okjsp/gawi/Game.java
@@ -19,8 +19,8 @@ public class Game implements Serializable  {
 		this.judgement = judgement;
 		this.datetime = datetime;
 	}
-
-	public Game() {
+	
+	public Game(){
 	}
 
 	public int getId() {
@@ -77,6 +77,14 @@ public class Game implements Serializable  {
 			}
 		}
 		return judgement;
+	}
+
+	public String getChoiceItem() {
+		return Play.items[this.choice];
+	}
+
+	public String getComputerChoiceItem() {
+		return Play.items[this.computerChoice];
 	}
 
 }
--- spring/mvc_/src/main/java/net/okjsp/gawi/GawiController.java
+++ spring/mvc_/src/main/java/net/okjsp/gawi/GawiController.java
@@ -2,14 +2,31 @@ package net.okjsp.gawi;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GawiController {
 	
+	@RequestMapping(value="/gawi.do", method=RequestMethod.GET)
+	public String gawi(Model model) {
+		return "gawi";
+	}
+
 	@RequestMapping(value="/query.do", method=RequestMethod.POST)
-	public String queryJSON(Model model) {
+	public String queryJSON(Model model, @ModelAttribute("game") Game game, @RequestParam("callback") String callback) {
+		Play play = new Play();
+		
+		game.setComputerChoice(play.getComputerChoice());
+		game.setJudgement(play.judge(game.getChoice(), game.getComputerChoice()));
+		play.save(game.getChoice(), game.getComputerChoice(), game.getJudgement());
+		Stat stat  = play.getStat();
+		
+		model.addAttribute("game", game);
+		model.addAttribute("stat", stat);
+		
 		return "queryJSON";
 	}
 	
